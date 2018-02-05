ef0a14acc226145414972c8b154a075219e4b5c8
Jason Thai <jasonnthaiii@gmail.com>
2018-02-04 21:50:09 -0800

    progress

--- hw2/hw2.scala
+++ hw2/hw2.scala
@@ -2,18 +2,28 @@
 
 object hw2 {
   def subs(s : String) : String = {
-    def subStrings(str : String) : String = {
-      str.head + "|" + str.head + str.tail.tail + "|" + subs(str.tail)
+    def subStringHelper(char1 : Char, str : String) : String = {
+      if (str.length() > 1){
+      char1 + str + "|" + subStringHelper(char1, str.tail) + "|" + subStringHelper(char1, str.head.toString())
+      }
+      else if (str.length() == 1){
+        char1 + str
+      }
+      else{
+        ""
+      }
+      
     }
-    if (s.length() == 0){
-     "" + "|"
+    
+    def subStringHelper2(str1 : String) : String = {
+     if (str1.length() > 1){
+       str1.head + "|" + str1.tail + "|" + subStringHelper2(str1.tail)
+      } 
+     else{
+        ""
+     }
     }
-    if (s.length() == 1){
-      s
-    }
-    else{
-      s + "|" + subStrings(s)  
-    }
+    subStringHelper2(s) + subStringHelper(s.head, s.tail);
   }
   
   def lcs(a : String, b : String) : String = {
