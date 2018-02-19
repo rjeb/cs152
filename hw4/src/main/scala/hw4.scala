e4318539138971881220860b2758d63b9ca0a176
Jason Thai <jasonnthaiii@gmail.com>
2018-02-18 21:22:19 -0800

    Current progress

--- hw4/src/main/scala/hw4.scala
+++ hw4/src/main/scala/hw4.scala
@@ -1,9 +1,63 @@
-
+import scala.util.matching._
 
 object hw4 extends App{
+  //Q1
+  def firstMatch(str: String, lst1: List[Regex]) : String = {
+		if (lst1.isEmpty){
+			null
+		}
+		else {
+			lst1.head.findPrefixOf(str).getOrElse(firstMatch(str, lst1.tail));
+		}
+	}
+  def tokens(input: String, list1 : List[Regex], list2 : List[Regex]) : (List[String], Int) = {
+    
+    def tokensHelper(input : String, patterns : List[Regex]) : List[String] = {
+  	  if (input.length == 0) Nil else {
+  		  val m = firstMatch(input, patterns)
+  		  if (m == null) Nil else m :: tokensHelper(input.substring(m.length), patterns)	
+  	  }
+    }
+    
+    def firstError(str: String, lst1: List[Regex], lst2: List[Regex], index: Int) : Int = {
+      if (str.isEmpty()){
+			  -1
+		  }
+		  else {
+		    if (firstMatch(str.head.toString(), lst1) == null && firstMatch(str.head.toString(), lst2) == null){
+		      return index;
+		    }
+		    else{
+		      firstError(str.tail, lst1, lst2, index+1)
+		    }
+		  }
+    }
+    
+    (tokensHelper(input, list1), firstError(input, list1, list2, 0))
+  }
+  
+  
+  //Q2
   val map1 = Map("2" -> "ABC", "3" -> "DEF", "4" -> "GHI", "5" -> "JKL", "6" -> "MNO", "7" -> "PRS", "8" -> "TUV", "9" -> "WXY");
+  
   val characters = (s: String) => s.toList.map("" +_);
-  val letters = map1.map(e => (e._1, characters(e._2)));
-  println(letters("3").flatMap(y => letters("2").map(x => x + y)));
+  
+  val letters ={
+    map1.map(e => (e._1, characters(e._2)))
+  }
+  
+  val cats = (s: List[String], t: List[String]) => {
+    t.flatMap(y => s.map(x => x + y))
+  }
+  
+  val wordsForDigits = (digits: String) => {
+    val lets = digits.map(c => letters(c.toString))
+    println(lets)
+    lets.reduceLeft(cats(_,_))
+  }
+  
+  println(cats(letters("2"), letters("3")).toSet);
+  println(wordsForDigits("23"))
+  
   
 }
--- hw4/src/test/scala/MyTestSuite.scala
+++ hw4/src/test/scala/MyTestSuite.scala
@@ -1,5 +1,21 @@
+import org.scalatest.FunSuite
+import scala.util.matching._
+import hw4._
 
-
-class MyTestSuite {
+class MyTestSuite extends FunSuite{
+  
+  test("Testing question 1 regex token function") {
+    val patterns = List("if|def|val".r, """\p{L}(\p{L}|\p{N}|_)*""".r,
+   """[+-]?\p{N}+""".r, "[+*/%<=>-]".r, "[(){};]".r)
+    val ignore = List("""\p{Z}+""".r)
+    val input = "if(x<0) 0 else root(x);"
+    val result = tokens(input, patterns, ignore)
+    assert(result == (List("if", "(", "x", "<", "0", ")"),-1))
+  }
+  
+  test("Testing list of concatenations of two strings") {
+    println(hw4.letters("2"))
+  }
+  
   
 }
