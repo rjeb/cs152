8223f416470fbc0bb0c02a26c287a0b45af818ed
Jason Thai <jasonnthaiii@gmail.com>
2018-02-26 13:12:30 -0800

    Finished lab7

--- lab7/src/main/scala/Main.scala
+++ lab7/src/main/scala/Main.scala
@@ -22,13 +22,21 @@ object Main extends App{
     case Operator(left, right, f) => f(eval(left, symbols), eval(right, symbols))
   }
   
-  //def eval2(expr: Expr)
+  def eval(d : Definition, symbols : Map[String, Int]) : Map[String, Int]=
+    symbols + (d.name -> eval(d.expr, symbols))
   
   val textExpr = Operator(Number(3), Operator(Number(4), Variable("x"), _*_), _+_)
   println("Tree for expression: " + textExpr)
   val answer = eval(textExpr, Map("x" -> 5))
   
   println(answer)
+  
+  val def1 = Definition("x", Number(2))
+  val def2 = Definition("y", Variable("x"))
+  val sym0 = Map[String, Int]()
+  val sym1 = eval(def1, sym0) // "x" -> 2
+  val sym2 = eval(def2, sym1) // "x" -> 2, "y" -> 2
+  System.out.println(sym2)
   
   
 }
