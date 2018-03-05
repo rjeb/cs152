42f319136793858e384e8f7878462f631c4c7c90
Jason Thai <jasonnthaiii@gmail.com>
2018-03-04 18:01:33 -0800

    Finished part1 and basic tests

--- hw5/src/main/scala/hw5part1.scala
+++ hw5/src/main/scala/hw5part1.scala
@@ -1,5 +1,48 @@
 
 
-class hw5part1 {
+object hw5part1{
+  abstract class Expr[T] {
+    def value: T
+  }
+  
+    case class Const[T](value : T) extends Expr[T]
+    //case class Variable(name : String) extends Expr[String]
+    
+    case class Op[T](fun : Seq[T] => T,  args : Expr[T]*) extends Expr[T] {
+      override def value: T = fun.apply(args.map(_.value))
+    }
+    
+    case class Sum(args : Expr[Int]*) extends Expr[Int]{
+      val intVals = args.map(_.value)
+      def recursiveSum(args1: Int*) : Int = {
+        if (args1.length == 0) 0
+        else args1.head + recursiveSum(args1.tail : _*)
+      }
+      
+      override def value = recursiveSum(intVals:_*)
+    }
+    
+    case class Product(args : Expr[Int]*) extends Expr[Int]{
+      val intVals = args.map(_.value)
+      def recursiveProduct(args1: Int*) : Int = {
+        if (args1.length == 0) 1
+        else args1.head * recursiveProduct(args1.tail : _*)
+      }
+      
+      override def value = recursiveProduct(intVals:_*)
+    }
+    
+  object Read extends Expr[Int]{
+    private val scanner = new java.util.Scanner(System.in)
+    def value : Int = scanner.nextInt()
+    
+  }
+  
+  
+  object Rand {
+    private val gen = new java.util.Random(42)
+    def value = gen.nextInt()
+  }
+  
   
 }
--- hw5/src/test/scala/OpSuite.scala
+++ hw5/src/test/scala/OpSuite.scala
@@ -3,12 +3,37 @@ import java.io._
 import hw5part1._
 
 class OpSuite extends FunSuite{
-  test("simple expression") {
+  test("simple expression Product") {
     assert(42 == Product(Const(6), Const(7)).value)
   }
+  
+  test("simple expression Sum") {
+    assert(13 == Sum(Const(6), Const(7)).value)
+  }
 
+  
   test("string op expression") {
     val cat: Expr[String] = new Op[String](_.mkString(""), Const("He"), Const("l"), Const("lo"))
     assert("Hello" == cat.value)
   }
+  
+  test("input expression") {
+    System.setIn(new ByteArrayInputStream(
+      "13".getBytes(java.nio.charset.StandardCharsets.UTF_8)))
+    System.setOut(new PrintStream(new ByteArrayOutputStream()))
+    assert(25 == Sum(Const(2), Const(10), Read).value)
+  }
+  
+   test("nestedExpr")
+   {
+      val p = Product(Const(2), Const(3))
+      assert(7 == Sum(Const(1), Const(p.value)).value);
+   }
+   
+   test("randomExpr")
+   {
+      assert(1029516620 == Sum(Const(Rand.value), Product(Const(Rand.value), Const(Rand.value))).value);
+   }
+  
+  
 }
