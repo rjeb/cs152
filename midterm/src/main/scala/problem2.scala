4f8079e2c049e4851d526c341fe41f8bec1f3e7c
Jason Thai <jasonnthaiii@gmail.com>
2018-03-21 12:20:07 -0700

    Problem 2

--- midterm/src/main/scala/problem2.scala
+++ midterm/src/main/scala/problem2.scala
@@ -1,17 +1,26 @@
 object problem2 extends App {
-  /*
+  
   def diffs(lst : List[Int]) = {
     val seed: (List[Int], Option[Int]) = (Nil, None)
     val foldResult = (lst :\ seed)((element, state) => 
-      ...
+        if (state._2 == None){
+          (state._1, Option(element))
+        }
+        else {
+          (List(element - state._2.get)++state._1 , Option(element))
+        }
       )
     foldResult._1
   }
-  
-  /*
-     Your diagram here...
-
+                
+  /*									(List(-6, 5), 2)
+   * 									/           	 \ 
+   * 							(List(-6), 7)				(2)
+   * 							/				\
+   * 				(Nil, 1)			(7)
+   * 			/				\
+   * (Nil, None)	(1)
    */
    println(diffs(List(1, 7, 2, 9)))
-   */
+   
 }
