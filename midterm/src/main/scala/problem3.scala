19f30facfcf79d9599f579398172c70009819a8e
Jason Thai <jasonnthaiii@gmail.com>
2018-03-21 12:30:01 -0700

    Problem 3 and 4

--- midterm/src/main/scala/problem3.scala
+++ midterm/src/main/scala/problem3.scala
@@ -1,8 +1,8 @@
 object problem3 extends App {
-  /*
-  def allCombinations(as: List[Int], bs: List[Int]) = ...
+  
+  def allCombinations(as: List[Int], bs: List[Int]) = {
+    as.flatMap(a => bs.map(b => (a*10 + b)))
+  }
   
   println(allCombinations(List(1, 2), List(3, 4, 5)))
-  * 
-  */
 }
--- midterm/src/main/scala/problem4.scala
+++ midterm/src/main/scala/problem4.scala
@@ -2,22 +2,32 @@ import scala.util.parsing.combinator._
 
 object problem4 extends App {
    class SimpleLanguageParser extends JavaTokenParsers {    
-     def expr: Parser[Int] = (term ~ opt(("+" | "-") ~ expr)) ^^ { 
+     def expr: Parser[Boolean] = (term ~ opt(("||") ~ expr)) ^^ { 
        case a ~ None => a
-       case a ~ Some("+" ~ b) => a + b
-       case a ~ Some("-" ~ b) => a - b
+       case a ~ Some("||" ~ b) => a || b
+       //case a ~ Some("-" ~ b) => a - b
      } 
     
-     def term: Parser[Int] = (factor ~ opt(("*" | "/") ~ term)) ^^ { 
+     def term: Parser[Boolean] = (factor ~ opt(("&&") ~ term)) ^^ { 
        case a ~ None => a
-       case a ~ Some("*" ~ b) => a * b
-       case a ~ Some("/" ~ b) => a / b
+       case a ~ Some("&&" ~ b) => a && b
+       //case a ~ Some("/" ~ b) => a / b
      }
      
-     def factor: Parser[Int] = wholeNumber ^^ (_.toInt) | "(" ~> expr <~ ")"
-  } 
+     def factor: Parser[Boolean] = "true" ^^ (x => true) | "false" ^^ (x => false) | notExpr/*wholeNumber ^^ (_.toInt)*/ | "(" ~> expr <~ ")"
+     def notExpr: Parser[Boolean] = ("!" ~> factor) ^^ {
+       case a => !a
+     }
+   } 
    
   val parser = new SimpleLanguageParser
   val result = parser.parseAll(parser.expr, " !true || !false && !!true")
-  println(result)         
+  val result1 = parser.parseAll(parser.expr, "true || false") // evals to true
+  val result2 = parser.parseAll(parser.expr, "true && false") // evals to false
+  val result3 = parser.parseAll(parser.expr, "false") // evals to false
+  val result4 = parser.parseAll(parser.expr, "true") // evals to true
+  val result5 = parser.parseAll(parser.expr, "!true") // evals to false
+  val result6 = parser.parseAll(parser.expr, "!false") // evals to true
+  
+  println(result)//given test case evals to true         
 }
