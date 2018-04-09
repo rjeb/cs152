eb30e287d76a9b48dc4a6fced73fe1beb99f39bf
Jason Thai <jasonnthaiii@gmail.com>
2018-04-08 21:14:16 -0700

    saving

--- hw8/mondrian.rkt
+++ hw8/mondrian.rkt
@@ -1,4 +1,5 @@
 #lang slideshow
+
 (define (randoms seed size)
   (if (= size 0)
   '()
@@ -31,21 +32,32 @@
 )
 
 (define (mondrian x y rands)
-  (if (< x 50)
-      (if (< y 50)
-          ;;choose a color at random
-          (if (< (first rands) .33)
-              (cc-superimpose (colorize (filled-rectangle x y) "skyblue") (rectangle (+ x 2) (+ y 2)))
-              (if (> (first rands) .66)
-                (cc-superimpose (colorize (filled-rectangle x y) "yellow") (rectangle (+ x 2) (+ y 2)))
-                (cc-superimpose (colorize (filled-rectangle x y) "red") (rectangle (+ x 2) (+ y 2)))
-                  ))
-          (values)
-          )
-      (values)
-  )
+    mondrianHelper(x y rands '())
 )
 
-(randoms 3023467485 40)
+(define (mondrianHelper x y rands returnLst)
+   
+  (letrec ([width (list x)]
+         [height (list y)]
+         (wide-enough (lambda (w r) (and (> w 50) (> (/ w width) (* 0.5 r)))))
+         (tall-enough (lambda (h r) (and (> h 50) (> (/ h height) (* 0.5 r)))))
+         (big-enough (lambda (w r1 h r2) (and (wide-enough) (tall-enough))))
+         (mondrian-helper (lambda (x y rands returnLst) '(1 2 3)))
+         )
+    (mondrian-helper x y rands returnLst)
+  )  
+  
+)
+
+;;following code is used to run eval
+(define-namespace-anchor anc)
+(define ns (namespace-anchor->namespace anc))
+
+;(randoms 3023467485 40)
 (mondrianSquare 40 40 '(.2))
-;;(mondrian 40 40 (randoms 3023467485 40))
+;(define x 5)
+;(define y 10)
+;(define expr (mondrian 40 40 (randoms 3023467485 40)))
+;(define expr '((cc-superimpose (colorize (filled-rectangle x y) "yellow") (rectangle (+ x 2) (+ y 2)))))
+;(eval expr ns)
+(mondrian 40 40 (randoms 3023467485 40))
