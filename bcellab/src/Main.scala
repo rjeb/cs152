4aac1c3fbe2bbc3971b708d7865f28fbb9ff82c5
Jason Thai <jasonnthaii@gmail.com>
2018-05-02 12:41:17 -0700

    lab 22

---
+++ bcellab/src/Main.scala
@@ -0,0 +1,37 @@
+import org.apache.bcel._
+import org.apache.bcel.generic._
+import org.apache.bcel.Constants._
+
+object Main extends App{
+  
+val cg = new ClassGen("HelloBCEL", "java.lang.Object", // Class, superclass
+  "(no filename)", ACC_PUBLIC | ACC_SUPER, null)
+val cp = cg.getConstantPool() // cg creates constant pool
+val il = new InstructionList()
+val factory = new InstructionFactory(cg)
+
+cg.addEmptyConstructor(ACC_PUBLIC) // if no constructor provided
+cg.getJavaClass().dump("HelloBCEL.class")
+
+val mg = new MethodGen(ACC_STATIC | ACC_PUBLIC, // access flags
+  Type.VOID, // return type
+  Array(new ArrayType(Type.STRING, 1)), // argument types
+  Array("argv"), // arg names
+  "main", "HelloBCEL", // method, class
+  il, cp) 
+
+il.append(InstructionFactory.createReturn(Type.VOID))
+mg.setMaxStack()
+cg.addMethod(mg.getMethod())
+il.dispose() // Allow instruction handles to be reused
+
+il.append(factory.createFieldAccess("java.lang.System", "out", 
+  new ObjectType("java.io.PrintStream"),
+  Constants.GETSTATIC))
+  
+il.append(factory.createConstant("Hello, World!"))
+
+il.append(factory.createInvoke("java.io.PrintStream", "println",
+  Type.VOID, Array(Type.INT), // return type, param types
+  Constants.INVOKEVIRTUAL))
+}
