6d4fb8600d7c7170f8a94df9ebec93945ba93616
Jason Thai <jasonnthaii@gmail.com>
2018-05-16 11:51:32 -0700

    Proper variances in header

--- final/problem4/src/main/scala/Table.scala
+++ final/problem4/src/main/scala/Table.scala
@@ -1,7 +1,6 @@
-class Table[K,+V](entries: List[(K,V)] = List()) { // TODO: Type variance
-  
+class Table[-K,+V](entries: List[(K,V)] = List()) { // TODO: Type variance
+  def sum(k: K, v : V) = 1;
+  def put(k : K,  v: V) = new Table((k, v) :: entries)
   def get(k: K) : Option[V] = None
   // TODO: The put method doesn't quite work. Make the appropriate
-  // changes.
-  def put(k: K, v: V): Table[K, V] = new Table((k, v) :: entries)
 }
