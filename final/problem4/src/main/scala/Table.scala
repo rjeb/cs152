64ef5b675ad581467eade59c41d7415077d97b5f
Jason Thai <jasonnthaii@gmail.com>
2018-05-16 11:53:07 -0700

    proper put

--- final/problem4/src/main/scala/Table.scala
+++ final/problem4/src/main/scala/Table.scala
@@ -1,6 +1,5 @@
 class Table[-K,+V](entries: List[(K,V)] = List()) { // TODO: Type variance
-  def sum(k: K, v : V) = 1;
-  def put(k : K,  v: V) = new Table((k, v) :: entries)
   def get(k: K) : Option[V] = None
+  def put(pair : Pair[K, V]) = 1;
   // TODO: The put method doesn't quite work. Make the appropriate
 }
